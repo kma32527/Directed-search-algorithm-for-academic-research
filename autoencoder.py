0763e6cd35d30a0cae847a6fde2729867324744e
kma32527 <59667128+kma32527@users.noreply.github.com>
2020-01-13 20:56:32 -0500

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -15,13 +15,13 @@ import json
 def findnearestn(rep, corpus_reps, n):
     distances=list()
     for i in range(len(corpus_reps)):
-        distances.append(dist.euclidean(rep, corpus_reps[i]))
+        distances.append(dist.cityblock(rep, corpus_reps[i]))
     distmap={}
     for index in range(len(distances)):
         distmap[str(index)]= -distances[index]
     return km.topncounts(distmap, n)
 
-def printnearestn(rep, input_text, n, corpus_titles, corpus_abstracts, feature_map, corpus_reps):
+def printnearestn(rep, input_text, input_abstract, n, corpus_titles, corpus_abstracts, feature_map, corpus_reps):
     gram_size=len(feature_map[0].split())
     if n>len(corpus_reps):
         print('Error: n is greater than the total number of files in the database.')
@@ -30,6 +30,7 @@ def printnearestn(rep, input_text, n, corpus_titles, corpus_abstracts, feature_m
     print('----------------------------')
     print('Input text:')
     print(input_text)
+    print(input_abstract)
     print('----------------------------')
     for i in range(len(matches)):
         index=int(matches[i][0])
@@ -40,7 +41,7 @@ def printnearestn(rep, input_text, n, corpus_titles, corpus_abstracts, feature_m
         print('------')
         try:
             print('Abstract:')
-#            print(corpus_abstracts[index])
+            print(corpus_abstracts[index])
         except:
             print('No abstract available.')
 
@@ -50,34 +51,36 @@ from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras import optimizers
 from tensorflow.keras.layers import GaussianDropout as noise
 
+skew=3
 def bintocat(featreps):
     newreps=list()
     for rep in featreps:
         temp=list()
         for i in rep:
             if i==0:
-                temp.append(1/3)
-                temp.append(5/3)
+                temp.append(1/skew)
+                temp.append(2-1/skew)
             else:
-                temp.append(5/3)
-                temp.append(1/3)
+                temp.append(2-1/skew)
+                temp.append(1/skew)
         newreps.append(temp)
     return newreps
 
 def invert(featreps):
     return 1-1*featreps
 
-f1='relu'
-f2='sigmoid'
+f_in='sigmoid'
+f_act='relu'
+f_out='sigmoid'
 def get_compiled_model(numvar):
     model = Sequential()
-    model.add(Dense(64, kernel_initializer='uniform', activation=f2, input_shape=(numvar,)))
-    model.add(Dense(16, kernel_initializer='uniform', activation=f1, input_shape=(64,)))
+    model.add(Dense(64, kernel_initializer='uniform', activation=f_in, input_shape=(numvar,)))
+    model.add(Dense(16, kernel_initializer='uniform', activation=f_act, input_shape=(64,)))
     model.add(noise(.1))
-    model.add(Dense(64, kernel_initializer='uniform', activation=f1, input_shape=(16,)))
-    model.add(Dense(numvar, kernel_initializer='uniform', activation=f2, input_shape=(64,)))
-    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.8, nesterov=True)
-    model.compile(loss='cosine_proximity', optimizer=sgd)
+    model.add(Dense(64, kernel_initializer='uniform', activation=f_act, input_shape=(16,)))
+    model.add(Dense(numvar, kernel_initializer='uniform', activation=f_out, input_shape=(64,)))
+    sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.8, nesterov=True)
+    model.compile(loss='logcosh', optimizer=sgd)
     return model
 
 ##import plos bio data
@@ -101,20 +104,20 @@ def get_compiled_model(numvar):
 
 ##extract ngram features
 
-#featmap, featreps=km.corpusmodel(cleanabs, 100, 500, 2)
+#featmap, featreps=km.corpusmodel(cleanabs, 200, 500, 2)
 #print(featmap)
 
 ##train and test autoencoder
     
 #i=3187 #Incorporating Domain Knowledge in Matching Problems via Harmonic Analysis
-i=4710 #Combining Evaluation Metrics via the Unanimous Improvement Ratio and its Application to Clustering Tasks
+#i=4710 #Combining Evaluation Metrics via the Unanimous Improvement Ratio and its Application to Clustering Tasks
 #i=7837 #Meta Reinforcement Learning with Latent Variable Gaussian Processes
-
+i=1783 #Toward Interpretable Topic Discovery via Anchored Correlation Explanation
 reps=bintocat(featreps)
 data=np.asarray(reps)
 model = get_compiled_model(len(reps[0]))
 model.fit(data, data, epochs=20,verbose=0)
 nnreps=model.predict(data)
 
-printnearestn(data[i], titles[i], 5, titles, rawabs, featmap, data)
-printnearestn(nnreps[i], titles[i], 5, titles, rawabs, featmap, nnreps)
+#printnearestn(data[i], titles[i], rawabs[i], 5, titles, rawabs, featmap, data)
+printnearestn(nnreps[i], titles[i], rawabs[i], 5, titles, rawabs, featmap, nnreps)
